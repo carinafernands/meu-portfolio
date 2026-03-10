a5d8fe291381e34255cd412516bde1dccd1c9ec5
Carina Fernandes <carina.fernandes@outlook.com>
2026-03-10 19:33:54 -0300

    Atualização de certificado

--- src/app/certificados/certificados/certificados.component.ts
+++ src/app/certificados/certificados/certificados.component.ts
@@ -13,43 +13,42 @@ export class CertificadosComponent {
       {
       titulo: "Administrador/Analista Salesforce",
       instituicao: "Udemy",
-      horas: "-",
+      descricao:"Texto",
+      link: "linux_conceitos_basicos.pdf"
+    },
+      {
+      titulo: "Linux - Conceitos e Principais comandos",
+      instituicao: "Udemy",
       link: "salesforce_adm_analista.pdf"
     },
     {
       titulo: "Programação BackEnd com JavaScript",
       instituicao: "Softex Pernambuco",
-      horas: "144",
       link: "FAP_Carina.pdf"
     },
     {
       titulo: "Angular Angular 19 - Curso completo do Iniciante ao Avançado",
       instituicao: "Udemy",
-      horas: "15",
       link: "angular_19.pdf"
     },
     {
       titulo: "Curso de Angular 2 (v17+) TypeScript do básico ao avançado",
       instituicao: "Udemy",
-      horas: "45",
       link: "angular_17.pdf"
     },
     {
       titulo: "Curso de JavaScript e TypeScript do básico ao avançado",
       instituicao: "Udemy",
-      horas: "146",
       link: "js_ts.pdf"
     },
     {
       titulo: "Python Essentials 1",
       instituicao: "Cisco",
-      horas: "-",
       link: "python_cisco.pdf"
     },
      {
       titulo: " Introdução a Cyber Segurança ",
       instituicao: "Cisco",
-      horas: "-",
       link: "Introduction_to_Cybersecurity_Badge20231109-29-y09w9p.pdf"
     }
 
